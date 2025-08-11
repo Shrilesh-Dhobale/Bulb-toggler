6fa266d7525f4250bb41a3e06a60f97629f21894
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-11 16:10:34 +0530

    Buttons added

---
+++ script.js
@@ -0,0 +1,8 @@
+function turnoff() {
+    document.getElementById("bulb-off");
+    document.getElementById("bulb-on");
+}
+function turnon() {
+    document.getElementById("bulb-on");
+    document.getElementById("bulb-off");
+}
