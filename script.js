6c95ca62424aff73c804286f16512dc572918e0b
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-11 16:37:05 +0530

    turnon fun created

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function turnoff() {
-    document.getElementById("bulb-off");
-    document.getElementById("bulb-on");
-}
-function turnon() {
-    document.getElementById("bulb-on");
-    document.getElementById("bulb-off");
+    const bodyElement = document.getElementById("body");
+    const cardElement = document.getElementById("card");
+    const headingElement = document.getElementById("heading");
+    const bulb = document.getElementById("bulb-off");
+    bodyElement.style.backgroundColor = "black";
+    cardElement.style.backgroundColor = "grey";
+    headingElement.style.color = "white";
+    bulb.src = "lightbulb.png";
 }
